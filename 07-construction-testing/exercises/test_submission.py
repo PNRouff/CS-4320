2ba1207ade68dfeebc200876cd4c3675ed8006a5
Rouff <39799526+PatrickRouff@users.noreply.github.com>
2020-10-13 11:12:09 -0500

    mass change for fixes

--- 07-construction-testing/exercises/test_submission.py
+++ 07-construction-testing/exercises/test_submission.py
@@ -17,16 +17,18 @@ def test_check_password(grading_system):
     grading_system.check_password('cmhbf5', 'bestTA')
 
 #Test 03 - broken
-def test_change_grade(grading_staff):
-    grading_staff.change_grade('akend3','comp_sci','assignment1',80)
+def test_change_grade(grading_system):
+    grading_system.login('cmhbf5', 'bestTA')
+    grading_system.usr.change_grade('akend3','comp_sci','assignment1',80)
 
 #Test 04 - broken
-def test_create_assignment(grading_staff):
-    grading_staff.create_assignment('assignmentTest','10/15/2020','comp_sci')
+def test_create_assignment(grading_system):
+    grading_system.usr.create_assignment('assignmentTest','10/15/2020','comp_sci')
 
 #Test 05
-def test_add_student(grading_professor):
-    grading_professor.add_student('pnrouff','comp_sci')
+def test_add_student(grading_system):
+    gradeSystem.login('goggins', 'augurrox')
+    grading_system.usr.add_student('pnrouff','comp_sci')
 
 #Test 06
 
@@ -35,12 +37,3 @@ def grading_system():
     gradingSystem = System.System()
     gradingSystem.load_data()
     return gradingSystem
-@pytest.fixture
-def grading_staff():
-    gradingStaff = Staff.Staff()
-    return gradingStaff
-@pytest.fixture
-def grading_professor():
-    gradingProfessor = Professor.Professor()
-    #gradingProfessor.__init__
-    return gradingProfessor
