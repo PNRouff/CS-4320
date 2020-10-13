080b8fb520397999337a629cfcdbd5ad802f4c8c
Rouff <39799526+PatrickRouff@users.noreply.github.com>
2020-10-13 12:16:01 -0500

    cleanup

--- 07-construction-testing/exercises/test_submission.py
+++ 07-construction-testing/exercises/test_submission.py
@@ -1,16 +1,13 @@
 import pytest
 import System
 
-
 #Test 01 - F
-#Tests if the program can handle a wrong username
 def test_login(grading_system):
     username = 'thtrhg'
     password =  'fhjhjdhjdfh'
     grading_system.login(username,password)
 
 #Test 02 - F
-#Tests if the password login can handle the wrong password
 def test_check_password(grading_system):
     grading_system.check_password('cmhbf5', 'bestTa')
 
