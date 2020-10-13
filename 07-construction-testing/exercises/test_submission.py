c8b10e911bd757371cf4569a7f579038fcc339ee
Rouff <39799526+PatrickRouff@users.noreply.github.com>
2020-10-13 11:13:33 -0500

    minor bug fixes for last major patch

--- 07-construction-testing/exercises/test_submission.py
+++ 07-construction-testing/exercises/test_submission.py
@@ -23,11 +23,12 @@ def test_change_grade(grading_system):
 
 #Test 04 - broken
 def test_create_assignment(grading_system):
+    grading_system.login('cmhbf5', 'bestTA')
     grading_system.usr.create_assignment('assignmentTest','10/15/2020','comp_sci')
 
 #Test 05
 def test_add_student(grading_system):
-    gradeSystem.login('goggins', 'augurrox')
+    grading_system.login('goggins', 'augurrox')
     grading_system.usr.add_student('pnrouff','comp_sci')
 
 #Test 06
