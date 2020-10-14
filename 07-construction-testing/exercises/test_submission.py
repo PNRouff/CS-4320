9e0b4bb679fdce9ae5a8d6ad385ec3d19c4990d1
Rouff <39799526+PatrickRouff@users.noreply.github.com>
2020-10-14 11:32:24 -0500

    start on 12

--- 07-construction-testing/exercises/test_submission.py
+++ 07-construction-testing/exercises/test_submission.py
@@ -76,18 +76,20 @@ def test_view_assignments(grading_system):
     grading_system.usr.view_assignments('databases')
 
 #Test 11 - F
+#Cannot submit to assignment you aren't on
 def test_submit_random_assignment(grading_system):
     grading_system.login('akend3', '123454321')
     grading_system.usr.submit_assignment('cloud_computing', 'assignment1','this should fail', '03/01/20')
 
 #Test 12 - F
+#Professors should not be able to add students to class
 def test_add_student_to_random_class(grading_system):
     grading_system.login('goggins', 'augurrox')
-    grading_system.usr.add_student('akend3','software_engineering')
+    grading_system.usr.add_student('akend3','cloud_computing')
     grading_system.login('akend3', '123454321')
     classes = grading_system.users['akend3']
     print(classes)
-    #if classes!=
+    #if classes=={'courses': {'comp_sci': {'assignment1': {'grade': 0, 'submission_date': '2/1/20', 'submission': 'Blah Blah Blah', 'ontime': True}, 'assignment2': {'grade': 66, 'submission_date': '2/8/20', 'submission': 'Blah2 Blah2 Blah2', 'ontime': True}}, 'software_engineering': {'assignment1': {'grade': 'N/A', 'submission_date': 'N/A', 'submission': 'N/A', 'ontime': 'N/A'}, 'assignment2': {'grade': 'N/A', 'submission_date': 'N/A', 'submission': 'N/A', 'ontime': 'N/A'}}}, 'password': '123454321', 'role': 'student'}:
     assert False
 
 
